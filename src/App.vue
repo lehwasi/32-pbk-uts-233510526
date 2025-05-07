e495cb92c7ee460c71014d1e313f73a86bdb80d6
lehwasi <ardinsyhm@gmail.com>
2025-05-07 19:33:56 +0700

    Tambahkan Checkbox untuk Menandai Selesai

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="app">
+  <div>
     <h1>To-Do List</h1>
-    <div class="input-section">
-      <input 
-        v-model="newTodo" 
-        @keyup.enter="addTodo" 
-        placeholder="Tambahkan kegiatan baru..."
-        class="task-input"
-      />
-      <button @click="addTodo" class="add-btn">Tambah</button>
-    </div>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Tambah tugas..." />
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input type="checkbox" v-model="todo.completed" />
+        <span :style="{ textDecoration: todo.completed ? 'line-through' : 'none' }">
+          {{ todo.text }}
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -23,13 +23,13 @@ export default {
   },
   methods: {
     addTodo() {
-      if (this.newTodo.trim() === '') return
+      if (this.newTodo.trim() === '') return;
       this.todos.push({
         id: Date.now(),
         text: this.newTodo,
         completed: false
-      })
-      this.newTodo = ''
+      });
+      this.newTodo = '';
     }
   }
 }
