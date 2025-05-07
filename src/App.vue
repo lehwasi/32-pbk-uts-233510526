f7d9a56b7c4aa312732807888de8b48a8e60b4bf
lehwasi <ardinsyhm@gmail.com>
2025-05-07 19:25:33 +0700

    Struktur Awal + Input Tambah Todo

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app">
+    <h1>To-Do List</h1>
+    <div class="input-section">
+      <input 
+        v-model="newTodo" 
+        @keyup.enter="addTodo" 
+        placeholder="Tambahkan kegiatan baru..."
+        class="task-input"
+      />
+      <button @click="addTodo" class="add-btn">Tambah</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      newTodo: '',
+      todos: []
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() === '') return
+      this.todos.push({
+        id: Date.now(),
+        text: this.newTodo,
+        completed: false
+      })
+      this.newTodo = ''
+    }
+  }
+}
+</script>
